f984008ceb6cb93668f82b154ab1d770b4efb1f8
guven <guvenkemal86@gmail.com>
2023-01-25 22:54:23 +0000

    revert to _app from app directory

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true
-  }
+  reactStrictMode: true
 };
-
 module.exports = (_phase, { defaultConfig }) => {
   const plugins = [withStaticImport, withBundleAnalyzer, withCustomWebpack];
   return plugins.reduce((acc, plugin) => plugin(acc), {
