b8c5795c54e165cf550eecc0e8a9fdf1b42f5b8b
guven <guvenkemal86@gmail.com>
2023-01-25 22:42:00 +0000

    build fix attempt

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
-  },
-}
+    appDir: true
+  }
+};
 
-module.exports = nextConfig
+module.exports = (_phase, { defaultConfig }) => {
+  const plugins = [withStaticImport, withBundleAnalyzer, withCustomWebpack];
+  return plugins.reduce((acc, plugin) => plugin(acc), {
+    ...defaultConfig,
+    ...config
+  });
+};
+
+module.exports = nextConfig;
